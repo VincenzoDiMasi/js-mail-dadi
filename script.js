e7fe3122524219c027625617359f868c28864e58
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-12 17:52:08 +0100

    Add html structure login exercise

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ buttonElement.addEventListener('click', function(){
         if(userNumber > cpuNumber){
             result = 'Hai vinto!';
         } else if (userNumber === cpuNumber){
-            result = 'Avete pareggiato!';
+            result = 'Abbiamo pareggiato!';
         }
 
        
@@ -51,3 +51,14 @@ buttonElement.addEventListener('click', function(){
         winnerElement.innerText = result;
 
 })
+
+//--------------------------------------------------------------------
+
+/*ESERCIZIO 2 - Finta Login
+Inventa una lista di email autorizzate
+Chiedi all’utente la sua email, con un piccolo form.
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Che ci sia un array da qualche parte?
+Cosa devo controllare esattamente?*/
