c68129ca8d460ea32aba2ff7f7730aaefe8d1ba5
VincenzoDiMasi <v.dimasi93@gmail.com>
2022-12-13 17:55:56 +0100

    Finish login exercise

--- script.js
+++ script.js
@@ -73,3 +73,28 @@ const emailList = ['robertdeniro@gmail.com', 'alpacino@gmail.com', 'leonardodica
 const emailElement = document.getElementById('email');
 const buttonElement = document.getElementById('login');
 const resultElement = document.getElementById('login-check-result');
+
+//Creo un event listener 
+buttonElement.addEventListener('click', function(){
+    //Prendo il value dell'input
+    const userEmail = emailElement.ariaValueMax;
+
+    let isAllowed = false;
+
+    //Verifico che la mail inserita sia valida 
+    for(let i = 0; i < emailList.length; i++){
+        const currentEmail = emailList[i];
+
+        if(currentEmail === userEmail){
+            isAllowed = true;
+        }
+    }
+
+    //Stampo in pagina
+    if(isAllowed){
+        resultElement.innerHTML = `<h2>La tua email è valida!`;
+    } else {
+        resultElement.innerHTML = `<h2>La tua email non è valida!`;
+
+    }
+})
